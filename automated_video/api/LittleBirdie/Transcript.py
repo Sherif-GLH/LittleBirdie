9d95120130e9c2e7eec29b546754814396be0d17
Sherif-GLH <sherifabouzeidgeeklabs@gmail.com>
2024-12-18 16:22:43 +0200

    fixing audio

--- automated_video/api/LittleBirdie/Transcript.py
+++ automated_video/api/LittleBirdie/Transcript.py
@@ -8,11 +8,11 @@ def adding_transcripts_audio(transcript_audio):
         local_filename = "downloads/sample.mp3"
 
 # Perform the GET request and download the file
-        with requests.get(url, stream=True) as response:
-            response.raise_for_status()  
-            with open(local_filename, "wb") as file:
-                for chunk in response.iter_content(chunk_size=8192):  
-                    file.write(chunk)
+        response = requests.get(url, stream=True) 
+        response.raise_for_status()  
+        with open(local_filename, "wb") as file:
+            for chunk in response.iter_content(chunk_size=8192):  
+                file.write(chunk)
         audio = AudioFileClip(local_filename)
         clip = audio.with_start(start_time)
         audio_list.append(clip) 
