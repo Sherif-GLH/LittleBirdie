3fd4e58fbc1f090e372f763be9d1d13fe7946359
Sherif-GLH <sherifabouzeidgeeklabs@gmail.com>
2024-12-18 14:27:17 +0200

    fixing audio

--- automated_video/api/tasks.py
+++ automated_video/api/tasks.py
@@ -1,7 +1,8 @@
 from .Main import create_video
 from celery import shared_task
-import requests
+import requests, subprocess
 from django.conf import settings
+from celery.app.control import Inspect
 
 @shared_task
 def create_video_task(intro, transcript_audio, content, video_name, metadata, webhook_url):
@@ -15,3 +16,18 @@ def create_video_task(intro, transcript_audio, content, video_name, metadata, we
         response = requests.post(webhook_url, json=payload)
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while sending video notification: {e}")
+    # check_tasks()
+
+def shutdown_instance():
+     subprocess.run(["sudo", "shutdown", "-h", "now"])
+
+def check_tasks():
+   active = Inspect.active()
+   reserved = Inspect.reserved()
+   scheduled = Inspect.scheduled()
+
+
+   if not any(active.values()) and not any(reserved.values()) and not any(scheduled.values()):
+     print("No tasks found in the queue...... shutting the instance down")
+     shutdown_instance()
+
