8c8cd7329a1f0355565fe7fc3787a30d0217873d
Sherif-GLH <sherifabouzeidgeeklabs@gmail.com>
2024-12-19 10:43:54 +0200

    testing audio

--- automated_video/api/LittleBirdie/Transcript.py
+++ automated_video/api/LittleBirdie/Transcript.py
@@ -1,11 +1,13 @@
 from moviepy import *
 import requests
+from Main import remove_local_file
 def adding_transcripts_audio(transcript_audio):
     audio_list = []
+    i = 0
     for item in transcript_audio:
         url = item["url"]
         start_time = item["start_time"]
-        local_filename = "downloads/sample.mp3"
+        local_filename = f"downloads/sample{i}.mp3"
 
 # Perform the GET request and download the file
         response = requests.get(url, stream=True) 
@@ -15,6 +17,8 @@ def adding_transcripts_audio(transcript_audio):
                 file.write(chunk)
         audio = AudioFileClip(local_filename)
         clip = audio.with_start(start_time)
-        audio_list.append(clip) 
+        audio_list.append(clip)
+        remove_local_file(local_filename) 
+        i += 1
     combined_intro_audio = CompositeAudioClip(audio_list)
     return combined_intro_audio
